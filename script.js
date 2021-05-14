ec3ba3dd993b993fb417695abaf9cc51a4ff5e94
Kirdun <52231692+Kirdun@users.noreply.github.com>
2021-05-14 15:35:15 +0300

    Файлы сайта

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.fas.fa-bars').addEventListener('click', () => {
+  document.querySelector('.submenu').classList.toggle('open');
+});
+document.querySelector('#polmenu').addEventListener('click', () => {
+  document.querySelector('.polmenu').classList.toggle('open');
+});
+document.querySelector('#stacmenu').addEventListener('click', () => {
+  document.querySelector('.stacmenu').classList.toggle('open');
+});
